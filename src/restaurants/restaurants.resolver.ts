fdaec39a5d7c45523cd20e493b9ade2a835c42ae
areum514 <21500514@handong.edu>
2020-11-22 21:47:13 +0900

    3.2 injecting the repository

--- src/restaurants/restaurants.resolver.ts
+++ src/restaurants/restaurants.resolver.ts
@@ -1,12 +1,16 @@
 import { Resolver,Query, Args, Mutation } from "@nestjs/graphql";
 import { CreateRestaurantDto } from "./dto/create-restaurant.dto";
 import { Restaurant } from "./entities/restaurant.entity";
+import { RestaurantService } from "./restaurants.service";
 
 @Resolver(of=>Restaurant)
 export class RestaurantResolver{
+  constructor(private readonly restaurantService:RestaurantService){
+    
+  }
 @Query(returns => [Restaurant]) 
-  restaurant(@Args('veganOnly') veganOnly:boolean):Restaurant[] { 
-    return [];
+  restaurant():Promise<Restaurant[]> { 
+    return this.restaurantService.getAll()
   }
 
   @Mutation(returns => Boolean)
